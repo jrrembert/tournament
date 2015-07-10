c4c5c36fc8d9cf0a7db7baa6771dc6a885f58dac
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2015-07-10 16:36:23 -0700

    Initial commit

---
+++ tournament/tournament.sql
@@ -0,0 +1,24 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+-- Create database
+CREATE DATABASE tournament;
+\c tournament
+
+-- Create tables
+CREATE TABLE player (
+	id integer,
+	name text,
+	wins integer,
+	losses integer);
+
+CREATE TABLE matches (
+	p1_id integer,
+	p2_id integer,
+	winner_id integer);
+	
