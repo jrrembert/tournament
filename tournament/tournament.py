026d088a51fe43645510231a6ec6753c0791000c
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2015-07-29 13:33:44 -0700

    Refactor views into sql file

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -133,46 +133,6 @@ def playerStandings():
     """
     db = connect('tournament')
     c = db.cursor()
-    matches_won_sql = ('CREATE OR REPLACE VIEW games_won AS '
-                       'SELECT players.id, players.name, '
-                       'COUNT(matches.match_id) AS wins '
-                       'FROM players '
-                       'LEFT JOIN matches '
-                       'ON players.id = matches.winner_id '
-                       'GROUP BY players.id;')
-    matches_lost_sql = ('CREATE OR REPLACE VIEW games_lost AS '
-                        'SELECT players.id, players.name, '
-                        'COUNT(matches.match_id) AS losses '
-                        'FROM players '
-                        'LEFT JOIN matches '
-                        'ON players.id = matches.loser_id '
-                        'GROUP BY players.id;')
-    matches_draw_sql = ('CREATE OR REPLACE VIEW games_draw AS '
-                        'SELECT players.id, players.name, '
-                        'COUNT(matches.match_id) AS draws '
-                        'FROM players '
-                        'LEFT JOIN matches '
-                        'ON players.id = matches.draw_id_one '
-                        'OR players.id = matches.draw_id_two '
-                        'GROUP BY players.id;')
-    total_matches_sql = ('CREATE OR REPLACE VIEW total_matches AS '
-                         'SELECT games_won.id, games_won.name, '
-                         'games_won.wins, games_lost.losses, '
-                         'games_draw.draws, '
-                         '(SELECT COALESCE(games_won.wins) + ' 
-                         'COALESCE(games_lost.losses) + '
-                         'COALESCE(games_draw.draws) AS matches) '
-                         'FROM games_won '
-                         'JOIN games_draw '
-                         'ON games_draw.id = games_won.id '
-                         'JOIN games_lost '
-                         'ON games_lost.id = games_draw.id '
-                         'ORDER BY games_won.wins DESC, '
-                         'games_draw.draws DESC;')
-    c.execute(matches_won_sql)
-    c.execute(matches_lost_sql)
-    c.execute(matches_draw_sql)
-    c.execute(total_matches_sql)
     c.execute('SELECT * FROM total_matches;')
     results = c.fetchall()
     db.commit()
